477de504ce56750f02532d690404c3970e73e36f
shubh-34 <144151402+shubh-34@users.noreply.github.com>
2024-11-19 15:38:13 +0530

    Add files via upload

---
+++ Ecom-proj/Ecom-proj/src/main/resources/data1.sql
@@ -0,0 +1,6 @@
+INSERT INTO product(id,name,desc,brand,price,category,release_Date,available,quantity)
+VALUES
+(1,'Tata Nexon','A Compact SUV with excellent safety features and performance','Tata Motors',750000,'Cars','2024-01-15',true,50),
+(2,'Maruti Suzuki','A popular hatchback known for its fuel efficiency and reliability','Maruti Suzuki',550000,'Cars','2024-02-01',true,100),
+(3,'Hyundai Creta','A stylish SUV with advanced features and comfortable interior','Hyundai',950000,'Cars','2024-03-01',true,75),
+(4,'Mahindra Thar','A rugged off-road SUVwith a powerful engine and modern aminities','Mahindra',1200000,'Cars','2024-04-01',true,30);
